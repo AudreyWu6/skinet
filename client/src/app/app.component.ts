78aecd863933fca6ce6f7b249a38ac16edc1a1c6
Jiayi Wu <wujiayi2017@gmail.com>
2025-02-07 18:40:35 +1100

    UI complete

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Product } from './models/product';
-import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +7,6 @@ import { Pagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: Product[] = [];
-  constructor(private http: HttpClient){
-
-  }
-  ngOnInit(): void {
-      this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
-        next: response => this.products = response.data,
-        error: error => console.log(error), // what to do when the request fails
-        complete: () => {
-          console.log('Request completed');
-        }
-      })
-  }
+  constructor(){ }
+  ngOnInit(): void { }
 }
